fbe9bbbc571e701b6e5daf2473002d884e2d0cad
Harry <harryndavies@pm.me>
2025-04-08 10:28:33 +0100

    feat: add vite config for federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,53 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import federation from '@originjs/vite-plugin-federation'
+import { fileURLToPath, URL } from 'url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
-    vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    federation({
+      name: 'remoteApp',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './HelloWorld': './src/components/HelloWorld.vue',
+        './HomeView': './src/views/HomeView.vue',
+        './AboutView': './src/views/AboutView.vue',
+      },
+      shared: ['vue'],
+    }),
   ],
+  server: {
+    port: 3001,
+    fs: {
+      allow: ['..'],
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    target: 'esnext', // Set the build target to esnext to support top-level await
+    minify: false, // Optional: disable minification for easier debugging
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        format: 'esm',
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
     },
   },
 })
